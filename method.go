23a8191fb45567dcd7c6ca1c57622f0ce8185ba0
eesher <eesher@126.com>
2017-12-31 19:00:57 +0800

    reflect the login method

--- method.go
+++ method.go
@@ -3,29 +3,28 @@ package main
 import (
 	"encoding/base64"
 	"encoding/json"
+	"reflect"
 	"strconv"
+	"strings"
 	"time"
 )
 
 var ERRCODE map[int]string
-var platform_method = make(map[string]func(map[string]string) (map[string]string, int))
 
 func ErrcodeInit() {
 	ERRCODE = make(map[int]string)
 	ERRCODE[0] = "ok"
 	ERRCODE[30001] = "db error"
 	ERRCODE[31001] = "invalid params"
-	ERRCODE[31002] = "invalid user"
+	ERRCODE[31002] = "invalid platform"
 	ERRCODE[31003] = "invalid passwd"
 	ERRCODE[31004] = "invalid device_id"
-	ERRCODE[32001] = "signup error"
+	ERRCODE[32001] = "login error"
 	ERRCODE[32002] = "registered user"
 	ERRCODE[33001] = "signin error"
 	ERRCODE[33002] = "can not found user"
 	ERRCODE[33003] = "passwd not match"
 	ERRCODE[33004] = "invalid token"
-
-	platform_method["guest"] = GuestLogin
 }
 
 var dur_time int64 = time.Date(2016, time.January, 0, 0, 0, 0, 0, time.UTC).Unix()
@@ -37,11 +36,16 @@ type Result struct {
 
 type LoginRet struct {
 	Result
+	LoginInfo
+}
+
+type LoginInfo struct {
 	Openid           string `json:"openid"`
 	Uid              string `json:"uid"`
 	ThirdOpenid      string `json:"third_openid"`
 	ThirdAccessToken string `json:"third_access_token"`
 	AccessToken      string `json:"access_token"`
+	DeviceID         string `json:"device_id"`
 	//	AdminLevel  int    `json:"admin_level"`
 }
 
@@ -65,52 +69,59 @@ func GetResult(errcode int) Result {
 	return Result{errcode, ERRCODE[errcode]}
 }
 
-func MakeUser(msg map[string]string) string {
+func (login_info *LoginInfo) MakeUser(msg map[string]string) int {
 	uid := time.Now().Unix() - dur_time
 	uid_str := strconv.FormatInt(uid, 16)
+
 	msg["uid"] = uid_str
 	msg["third_openid"] = msg["device_id"]
 	msg["name"] = "default name"
 
 	//var json_data []byte
-	if NewUserMap(msg) != 0 {
-		return ""
+	errcode := 0
+	if errcode = NewUserMap(msg); errcode != 0 {
+		return errcode
 	}
-	if NewUser(msg) != 0 {
-		return ""
+	if errcode = NewUser(msg); errcode != 0 {
+		return errcode
 	}
 
-	return uid_str
+	login_info.Uid = uid_str
+	return errcode
 }
 
-func GuestLogin(msg map[string]string) (map[string]string, int) {
-	ret := make(map[string]string)
-	uid, errcode := GetUid("guest", msg["device_id"])
+func (login_info *LoginInfo) GuestLogin(msg map[string]string) int {
+	var errcode int
+	login_info.Uid, errcode = GetUid("guest", msg["device_id"])
 	if errcode != 0 {
-		return ret, errcode
+		return errcode
 	}
-	if len(uid) == 0 {
-		uid = MakeUser(msg)
+	if len(login_info.Uid) == 0 {
+		if errcode = login_info.MakeUser(msg); errcode != 0 {
+			return errcode
+		}
 	}
-	ret["uid"] = uid
-	ret["third_openid"] = msg["device_id"]
-	return ret, 0
+	login_info.ThirdOpenid = msg["device_id"]
+	return 0
 }
 
 func Login(msg map[string]string, json_data *[]byte) {
-	var ret map[string]string
-	var errcode int
-	if method, ok := platform_method[msg["platform"]]; ok {
-		ret, errcode = method(msg)
-		if errcode != 0 {
-			*json_data, _ = json.Marshal(GetResult(errcode))
-			return
-		}
+	//var errcode int
+	login_info := LoginInfo{}
+	method := reflect.ValueOf(&login_info).MethodByName(strings.Title(msg["platform"]) + "Login")
+	if !method.IsValid() {
+		*json_data, _ = json.Marshal(GetResult(31002))
+		return
+	}
+	ret := method.Call([]reflect.Value{reflect.ValueOf(msg)})
+	if errcode := ret[0].Int(); errcode != 0 {
+		*json_data, _ = json.Marshal(GetResult(int(errcode)))
+		return
 	}
 
-	ret["device_id"] = msg["device_id"]
-	access_token := base64.URLEncoding.EncodeToString(GenerateToken(ret["uid"]))
-	*json_data, _ = json.Marshal(LoginRet{GetResult(0), ret["openid"], ret["uid"], ret["third_openid"], ret["third_access_token"], access_token})
+	login_info.DeviceID = msg["device_id"]
+	login_info.AccessToken = base64.URLEncoding.EncodeToString(GenerateToken(login_info.Uid))
+	*json_data, _ = json.Marshal(LoginRet{GetResult(0), login_info})
 }
 
 func Auth(msg map[string]string, json_data *[]byte) {
